37fd14bf2d3694b4f74d9243e39a2cd4fe6d1300
Nikitha75 <nikithanikki474@gmail.com>
2023-08-16 23:37:18 +0530

    Added about page and styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Wrapper from "../assets/wrappers/AboutPage";
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <Wrapper>
+      <h3>About us</h3>
+      <p>
+        Introducing YumazingBites, the ultimate go-to app for recipes. With a
+        flick of your finger, you'll unlock a treasure trove of delicious
+        recipes.
+        <br />
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam
+        repellendus quo libero odit non possimus distinctio quibusdam earum at
+        enim qui esse iusto tenetur dolor reiciendis, recusandae inventore animi
+        optio temporibus nam ipsam obcaecati rem! Aliquid, deserunt nemo aliquam
+        nam sunt repudiandae iusto inventore voluptatum atque, facilis provident
+        porro placeat odit corporis soluta explicabo sit laudantium, ullam culpa
+        odio molestiae quis tempora! Soluta corporis, possimus doloribus nisi
+        esse ipsum odio illo ad deserunt iusto at odit alias! Earum sunt vitae,
+        aperiam molestiae omnis impedit? Beatae obcaecati non, tempora tenetur a
+        sapiente praesentium deserunt corrupti nulla, ratione, cumque ad esse
+        facere.
+      </p>
+    </Wrapper>
+  );
 };
 
 export default About;
