5a23434e5ff8c83af336cf83bb59659550c4f57e
Nikitha75 <nikithanikki474@gmail.com>
2023-08-26 20:58:16 +0530

    Add error statement

--- src/pages/SinglePageError.jsx
+++ src/pages/SinglePageError.jsx
@@ -4,7 +4,8 @@ import { useRouteError } from "react-router-dom";
 const SinglePageError = () => {
   const error = useRouteError();
   console.log(error);
-  return <h2>{error.message}</h2>;
+  // return <h2>{error.message}</h2>;
+  return <h2>There's an error...</h2>;
 };
 
 export default SinglePageError;
