d18ecf24b0d91f7864dc33b9622c2e37aa8e256e
Nikitha75 <nikithanikki474@gmail.com>
2023-09-13 22:53:01 +0530

    Pass in queryClient to landing loader

--- src/App.jsx
+++ src/App.jsx
@@ -32,7 +32,7 @@ const router = createBrowserRouter([
         index: true,
         element: <Landing />,
         errorElement: <SinglePageError />,
-        loader: landingLoader,
+        loader: landingLoader(queryClient),
       },
       {
         path: "recipe/:id",
