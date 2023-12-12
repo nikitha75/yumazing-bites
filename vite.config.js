d301e6aabfb87343bad95bb34499eb62ab0e0c49
Nikitha75 <nikithanikki474@gmail.com>
2023-12-12 15:07:05 +0530

    Add configuartion

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://yumazing-bites-api.onrender.com/api",
+        // target: "http://localhost:5100/api",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
